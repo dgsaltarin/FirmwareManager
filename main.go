f1d02130e7bea4c1c600582c5e1087387044319b
daniel saltarin castillo <dgsaltarin@gmail.com>
2023-10-21 13:45:35 -0500

    added healthcheck function

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/dgsaltarin/SharedBitesBackend/dc-nearshore/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+type RequestBody struct {
+	Image string
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/healthcheck", controllers.HealthCheck())
+	// router.POST("/texttrack", controllers.UploadImage())
+	// router.POST("/users", controllers.CreateUser())
+	// router.GET("/users", controllers.GetUserByUsername())
+	// router.POST("/login", controllers.Login())
+	// router.POST("/signup", controllers.SignUp())
+
+	router.Run()
+}
